93c23db08044fea6640263355f8f2a38904ac9ff
Olha Dobrynina <dobrynina.olha@gmail.com>
2026-01-31 12:23:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,91 @@
-print(1, end='') 
+chars = ['a', 'b', 'c']
+numbers = [1, 2]
+chars.extend(numbers)
+print(chars)  # Output: ['a', 'b', 'c', 1, 2]
+
+chars = ['a', 'c']
+chars.insert(1, 'b')
+print(chars)  # Output: ['a', 'b', 'c']
+
+chars = ['a', 'b', 'c']
+chars.clear()
+print(chars)  # Output: []
+
+my_list = [1, 'Hello', 3.14]
+my_list.append(7)
+print(my_list)  # Output: [1, 'Hello", 3.14, 4]
+           
+x = [1, 2, 3] 
+x[1] = -2
+print(x)
+
+x = ['a', 'b', 'c']
+y = x.index('c')
+print(y)  # Output: 2
+
+my_list = [1, 2, 3, 4, 2, 2, 5, 2]
+count_2 = my_list.count(6)
+print(count_2)  # Output: 4
+
+x = [1, 2, 3, 4, 5]
+print(len(x))
+
+x = [3,1,4,1,5,9,2]
+x.sort()
+print(x)
+
+x = [3,1,4,1,5,9,2]
+x.sort(reverse=True)
+print(x)
+
+x = ["apple", "banana", "cherry"]
+x.sort(key=len)
+print(x)  # Output: ['apple', 'banana', 'cherry']
+
+nums = [3,1,4,1,5,9,2]
+sorted_nums = sorted(nums)
+print(sorted_nums)
+
+nums = [3,1,4,1,5,9,2]
+sorted_nums_desc = sorted(nums, reverse=True)
+print(sorted_nums_desc)
+
+words = ["banana", "apple", "cherry"]
+sorted_words = sorted(words, key=len)
+print(sorted_words)  # Output: ['apple', 'banana', 'cherry']
+
+chars = ['a', 'b']
+chars_copy = chars.copy()
+print(chars_copy)  # Output: ['a', 'b']
+
+chars = ['banana', 'apple', 'cherry']
+chars.reverse()
+print(chars)  # Output: ['cherry', 'apple', 'banana']
+
+my_dict = {"name":"Alice", "age":25}
+print(my_dict["name"])  # Output: Alice
+my_dict["age"] = 28
+print(my_dict)
+my_dict["age"] = 32
+print(my_dict)  # Output: {'name': 'Alice', 'age': 32}
+my_dict["email"] ="alice@example.com"
+print(my_dict)  # Output: {'name': 'Alice', 'age': 32, 'email': '
+
+my_dict["surname"] = "Dobrynina"
+print(my_dict)
+my_dict["citizenship"] = "Ukraine"
+print(my_dict)  # Output: {'name': 'Alice', 'age': 32, 'email': '
+my_dict["name"] = "Olha"
+print(my_dict)
+print("name" in my_dict)  # Output: True
+print("age" in my_dict)
+git add main.py  # Output: True
+git commit -m "Update main.py"  # Output: True
+
+
+ 
+
+
+
+
+
